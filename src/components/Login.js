f1eb91bf39537604ffe5e10217ffac778a95b82c
David Shams <david.shams@gmail.com>
2017-12-09 14:51:48 -0600

    Login Form added - working

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,27 @@ class Login extends Component {
     return (
       <div className="Login">
       <h3>Admin Login</h3>
+      <Form />
       </div>
     );
   }
 }
 
+const Card = (props)=> {
+
+}
+let agents = ['David', 'Mike', 'Bot'];
+
+class Form extends React.Component{
+    render(){
+        return(
+            <form>
+                <h4>Login Form</h4>
+                <span>AAA </span><input type="text"/>
+                <span>Sine </span><input type="text"/>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
 export default Login;
