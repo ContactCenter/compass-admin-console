f5bb0febec36f4155639113ca1b168e668786118
David Shams <david.shams@gmail.com>
2017-12-09 15:05:03 -0600

    unprakin believeable dynamic data pulled by React easily - works

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import './Login.css';
 
-class Login extends Component {
+class Login extends React.Component{
   render() {
     return (
       <div className="Login">
@@ -11,17 +11,40 @@ class Login extends Component {
     );
   }
 }
-
 const Card = (props)=> {
+    return(
+        <div>
+            <img src={props.agent_url} alt=""/>
+            <div>{props.name}</div>
+        </div>
+    )
+};
+let agents = [
+    {   "name": "David Shams",
+        "agent_url": "https://avatars0.githubusercontent.com/u/7882227?s=40&v=4"
+    },
+    {   "name": "Mike Knott",
+        "agent_url": "https://avatars0.githubusercontent.com/u/7882227?s=40&v=4"
+    },
+    {   "name": "John Doe",
+        "agent_url": "https://avatars0.githubusercontent.com/u/7882227?s=40&v=4"
+    },
+    ];
 
-}
-let agents = ['David', 'Mike', 'Bot'];
-
+const AgentList = (props)=> {
+        return (
+            <div className="Login">
+                <h3>AgentList</h3>
+                <div>{props.cards.map(card => <Card {...card}/>)}</div>
+            </div>
+        );
+};
 class Form extends React.Component{
     render(){
         return(
             <form>
                 <h4>Login Form</h4>
+                <AgentList cards={agents}/>
                 <span>AAA </span><input type="text"/>
                 <span>Sine </span><input type="text"/>
                 <button type="submit">Submit</button>
